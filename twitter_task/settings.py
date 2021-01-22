dffdf1fd9a602c4410a3db7cac447475f37dc9d2
hardik2020 <citytravel.139@gmail.com>
2021-01-22 23:40:22 +0530

    Hosting related commit

--- twitter_task/settings.py
+++ twitter_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '18bj(*!rj)(fxbog$v3$pd1t%^$ecrb@l6ol_flit#!-bjpr0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','twitterseva.herokuapp.com']
 
