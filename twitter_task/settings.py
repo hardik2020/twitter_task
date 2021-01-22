61c130764f967cfc246540e8b7abad384fb6f039
hardik2020 <citytravel.139@gmail.com>
2021-01-22 22:44:32 +0530

    Hosting related commit

--- twitter_task/settings.py
+++ twitter_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '18bj(*!rj)(fxbog$v3$pd1t%^$ecrb@l6ol_flit#!-bjpr0%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://twitter-seva.herokuapp.com/']
 
 
 # Application definition
